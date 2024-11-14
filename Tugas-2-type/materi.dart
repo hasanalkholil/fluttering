084ccef8dce1e2b323a7ce3e85a820f6636cb470
hasanal fachri <hasanal.fachri12@gmail.com>
2024-11-14 17:22:03 +0700

    Second Commit- tugas type data

---
+++ Tugas-2-type/materi.dart
@@ -0,0 +1,17 @@
+/*void main(){
+  print('hello world');
+} */
+
+
+/* void main(){
+  print("masukkan password : ");
+  String inputText = stdin.readLineSync();
+  print("password : " + inputText);
+}*/
+
+
+void main(){
+for (int i = 0; i < 10; i++) {
+    print('hello ${i + 1}');
+  }
+}
---
+++ Tugas-2-type/type.dart
@@ -0,0 +1,64 @@
+// Soal Nomor 1 
+ /*main() {
+  var word = 'dart';
+  var second = 'is';
+  var third = 'awesome';
+  var fourth = 'and';
+  var fifth = 'I';
+  var sixth = 'love';
+  var seventh = 'it!';
+
+  print('$word $second $third $fourth $fifth $sixth $seventh');
+}
+*/
+
+
+
+
+
+// ======================================
+//Soal Nomor 2
+/*void main() {
+  var sentence = "I am going to be Flutter Developer";
+
+  var exampleFirstWord = sentence[0]; // "I"
+  var exampleSecondWord = sentence.substring(2, 4); // "am"
+  var thirdWord = sentence.substring(5, 10); // "going"
+  var fourthWord = sentence.substring(11, 13); // "to"
+  var fifthWord = sentence.substring(14, 16); // "be"
+  var sixthWord = sentence.substring(17, 24); // "Flutter"
+  var seventhWord = sentence.substring(25); // "Developer"
+
+  print('First word: $exampleFirstWord');
+  print('Second word: $exampleSecondWord');
+  print('Third word: $thirdWord');
+  print('Fourth word: $fourthWord');
+  print('Fifth word: $fifthWord');
+  print('Sixth word: $sixthWord');
+  print('Seventh word: $seventhWord');
+}*/
+
+
+
+
+
+// ======================================
+//Soal Nomor 3
+
+/*import 'dart:io';
+
+void main() {
+  // Tanyakan nama depan
+  stdout.write("Masukkan nama depan: ");
+  String? firstName = stdin.readLineSync();
+
+  // Tanyakan nama belakang
+  stdout.write("Masukkan nama belakang: ");
+  String? lastName = stdin.readLineSync();
+
+  // combine keduanya
+  String fullName = "$firstName $lastName";
+
+  // tampilkan nama lengkap
+  print("Nama lengkap Anda adalah: $fullName");
+}*/
